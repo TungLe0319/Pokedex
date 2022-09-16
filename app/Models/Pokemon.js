3471e885eda3414dbf1376716df5fa5fa4482fad
Tung Le <tung.le0319@gmail.com>
2022-09-15 20:14:23 -0600

    3

--- app/Models/Pokemon.js
+++ app/Models/Pokemon.js
@@ -1,24 +1,27 @@
+import { PokemonDetail } from './PokemonDetail.js';
+
 export class Pokemon {
   constructor(data) {
+    this.id = data.url
+      .replace('https://pokeapi.co/api/v2/pokemon/', '')
+      .replace('/', '');
     this.name = data.name;
     this.url = data.url;
+    this.img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${this.id}.png`;
   }
 
   get CardTemplate() {
     return /*html */ `
    <div class="  p-2 text-light selectable  " onclick="app.pokemonsController.getPokemonDetails('${this.url}')">
              
-              
+              <img src="${this.img}" alt="" style="width:5rem;height:5rem;">
               <img src="/resources/pokeball.png" alt="" style="width:1rem;" class="me-1"> ${this.name}
              
             </div>
     `;
   }
 
-
-
-  
-
+  get pokemonimg() {}
   get WildTemplate() {}
 
   get ActiveTemplate() {}
